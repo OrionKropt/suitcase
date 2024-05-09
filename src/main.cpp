a9a3f849d109f34b1a57166e37d09e7d7e52ccb4
Kropt <SlavaCh2018@yandex.ru>
2024-05-09 13:20:49 +0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <modbus.h>
+#include "registers.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+	modbus_t* ctx;
+	ctx = modbus_new_rtu("\COM2", 19200, 'N', 8, 1);
+    if (ctx == NULL) {
+        std::cout << "Не удалось создать контекст Modbus" << std::endl;
+        return 1;
+    }
+
+    // Установка адреса устройства 
+    modbus_set_slave(ctx, 1);
+
+    // Установка на RS-485
+    modbus_rtu_set_serial_mode(ctx, MODBUS_RTU_RS485);
+
+    // Подключение к устройству 
+    if (modbus_connect(ctx) == -1) {
+        std::cout << "Не удалось подключиться к устройству" << std::endl;
+        modbus_free(ctx);
+        return 1;
+    }
+    else cout << "Успешное подключение\n";
+   
+
+
+    int reg_count = 2;
+    int buffer_size = 2;
+                                            // Чтение данных из регистров 
+    uint16_t regs[10];                      // Для хранения данных из 2-х регистров 
+    int rc = modbus_read_registers(
+        ctx,
+        REAL_ENERGY_TOTAL_FLOAT,                               // Адресс первого регистра
+        reg_count,                                  // Количестов регистров
+        regs);
+    cout << rc << endl;
+    if (rc == -1) {
+        std::cout << "Ошибка чтения регистров" << std::endl;
+       // cout << modbus_strerror(errno) << endl;
+        modbus_close(ctx);
+        modbus_free(ctx);
+        return 1;
+    }
+
+    float reg_content = modbus_get_float(regs);
+    //float reg_content = modbus_get_float_dcba(regs);
+
+    cout << "Значение регистра " << reg_content << endl;
+    // Вывод данных 
+    /*for (int i = 0; i < buffer_size; ++i) {
+        std::cout << "Значение регистра " << i << ": " << regs[i] << std::endl;
+    }*/
+
+    // Закрытие соединения и освобождение контекста 
+    modbus_close(ctx);
+    modbus_free(ctx);
+
+	return 0;
+}
---
+++ src/registers.h
@@ -0,0 +1,17 @@
+#pragma once 
+
+// Metered data
+
+#define REAL_ENERGY_TOTAL_FLOAT 1000    // kWh
+
+
+
+// Setup ans status
+
+#define USAGE_HOURS_INT 4110            // TIMER
+#define USAGE_MINUTES_INT 4111          // TIMER
+
+#define SERVICE_FRIQUENCY_INT 4125      // 50 or 60 Hz
+
+#define DISPLAY_MODE_INT 4128           // 0 = IEC
+                                        // 1 = IEEE
