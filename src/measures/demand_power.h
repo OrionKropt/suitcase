09c9bdb5701d208b6c28b0d5ee73d024bc7b0d4f
Kropt <SlavaCh2018@yandex.ru>
2024-05-11 22:34:51 +0500

    Changed power.h, changed read_power(), changed error output

--- src/measures/demand_power.h
+++ src/measures/demand_power.h
@@ -1,15 +1,49 @@
 #pragma once
 
 #include <modbus.h>
+#include <unordered_map>
+
+struct Power_int_registers
+{
+	uint16_t phase_3;
+	uint16_t A;
+	uint16_t B;
+	uint16_t C;
+	uint16_t N;
+	uint16_t total;
+	uint16_t max;
+	uint16_t min;
+	uint16_t last;
+	uint16_t peak;
+};
+
+
+struct Power_float_registers
+{
+	uint16_t phase_3;
+	uint16_t A;
+	uint16_t B;
+	uint16_t C;
+	uint16_t N;
+	uint16_t total;
+	uint16_t max;
+	uint16_t min;
+	uint16_t last;
+	uint16_t peak;
+};
 
 class Demand_power
 {
 public:
-	uint16_t phase_3[2];
+	std::unordered_map<uint16_t, int16_t[2]> data;
 
-	uint16_t last[2];
-	uint16_t peak[2];
-	
 
-	uint16_t total[2];
+	//int16_t phase_3[2];
+
+	//int16_t last[2];
+	//int16_t peak[2];
+	//int16_t total[2];
+
+	Power_float_registers int_regs;
+	Power_int_registers float_regs;
 };
