056e977790583897157ddf9a6a86bd2c9e829824
Kropt <SlavaCh2018@yandex.ru>
2024-05-11 20:34:33 +0500

    added reading int data, added some registers, added output error

--- src/data_logger.cpp
+++ src/data_logger.cpp
@@ -56,15 +56,113 @@ auto Data_logger::init() -> void
 		std::cout << "Успешное подключение!\n";
 		#endif // DEBUG
 	}
+
+	is_float = false;
 }
 
 auto Data_logger::read_data() -> void
 {
+	read_power();
+}
+
+auto Data_logger::write_data() -> void
+{
+	
+#ifdef DEBUG
+	std::cout << "\nPower\n";
+	std::cout << power.real.name << ", kW" << '\n';
+	std::cout << "A\tB\tC\n";
+	if (!is_float)
+	{
+		std::cout << power.real.B[0] << "\t"
+			<< power.real.B[0] << "\t"
+			<< power.real.C[0] << '\n';
+		std::cout << "Всего " << power.real.total[0] << " kW\n";
+		std::cout << "Min = " << power.real.min[0] << " kW\n";
+		std::cout << "Max = " << power.real.max[0] << " kW\n";
+	}
+	else
+	{
+		float temp = modbus_get_float(power.real.min);
+		std::cout << "Mim = " << temp << " kW\n";
+	}
+#endif // DEBUG
 
 }
 
 auto Data_logger::read_power() -> int
 {
-	
-	return 0;
+	if (-1 == read_real_power(is_float))
+	{
+		return -1;
+	}
+
+
+
 }
+
+auto Data_logger::register_read_error(uint16_t reg) -> void
+{
+#ifdef DEBUG
+	std::cout << "Ошибка чтения регистров\n";
+	std::cout << "Регистр " << reg << '\n';
+#endif // DEBUG
+
+}
+
+auto Data_logger::read_real_power(bool is_float) -> int
+{
+	if (!is_float)
+	{	
+		// ---------INT----------
+
+		// phase reading
+
+		if (-1 == modbus_read_registers(ctx, REAL_POWER_A_INT, 3, buf))
+		{
+			register_read_error(REAL_POWER_A_INT);
+			return -1;
+		}
+		power.real.A[0] = buf[0];
+		power.real.B[0] = buf[1];
+		power.real.C[0] = buf[2];
+
+
+		if (-1 == modbus_read_registers(ctx, REAL_POWER_TOTAL_INT, 1, buf))
+		{
+			register_read_error(REAL_POWER_TOTAL_INT);
+			return -1;
+		}
+		power.real.total[0] = buf[0];
+
+		// min max
+		if (-1 == modbus_read_registers(ctx, REAL_POWER_TOTAL_MIN_INT, 1, buf))
+		{
+			register_read_error(REAL_POWER_TOTAL_INT);
+			return -1;
+		}
+		power.real.min[0] = buf[0];
+		if (-1 == modbus_read_registers(ctx, REAL_POWER_TOTAL_MAX_INT, 1, buf))
+		{
+			register_read_error(REAL_POWER_TOTAL_INT);
+			return -1;
+		}
+
+		power.real.max[0] = buf[0];
+		// ---------INT----------
+		return 0;
+	}
+	else
+	{
+		// min max
+		if (-1 == modbus_read_registers(ctx, REAL_POWER_TOTAL_MIN_FLOAT, 2, buf))
+		{
+			register_read_error(REAL_POWER_TOTAL_INT);
+			return -1;
+		}
+		memcpy_s(power.real.min, sizeof(uint16_t) * 2, buf, sizeof(uint16_t) * 2);
+		return 0;
+	}
+}
+
+
--- src/measures/real_power.h
+++ src/measures/real_power.h
@@ -5,16 +5,20 @@
 class Real_power
 {
 public:
+
+	static const char* name;
+
 	uint16_t phase_3[2];
 
-	uint16_t A[2];
-	uint16_t B[2];
-	uint16_t C[2];
+	uint16_t A[2]{ 0 };
+	uint16_t B[2]{ 0 };
+	uint16_t C[2]{ 0 };
 
-	uint16_t total[2];
+	uint16_t total[2]{ 0 };
 
-	uint16_t max[2];
-	uint16_t min[2];
+	uint16_t max[2]{ 0 };
+	uint16_t min[2]{ 0 };
 
 	Demand_power demand;
 };
+
