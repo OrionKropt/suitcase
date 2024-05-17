5167d875d4c690cf5d429a3746390590376e691e
Larts <alex1790007@mail.ru>
2024-05-17 22:26:38 +0500

    Tiny style changes

--- src/error.cpp
+++ src/error.cpp
@@ -1,11 +1,10 @@
 #include "error.h"
+
 #include <iostream>
 
-
 auto print_error(const char* file, unsigned int line, const char* function,
-	const char* what, bool exit, const char* extra_print) -> void
+	             const char* what, bool exit, const char* extra_print) -> void
 {
-
 	std::cerr << "ERROR: \'" << what << "\'\n";
 	std::cerr << "\tIn " << function << '\n';
 	std::cerr << '\t' << file << ", line " << line << '\n';
--- src/error.h
+++ src/error.h
@@ -2,11 +2,10 @@
 
 #include <format>
 
-						// Becouse it's inpossible to put const char str whit macro
-#define EMPTY_STR ""    //-------------------------------------------------------|
-																			//   |
-auto print_error(const char* file, unsigned int line, const char* function, //   |
-	const char* what, bool exit, const char* extra_print) -> void;          //   |
-																			//  \ /
+#define EMPTY_STR ""
+
+auto print_error(const char* file, unsigned int line, const char* function,
+	             const char* what, bool exit, const char* extra_print) -> void;
+
 #define PRINT_ERROR(what, exit, ...) \
-	{print_error(__FILE__, __LINE__, __FUNCTION__, (what), (exit), std::format(EMPTY_STR __VA_ARGS__).c_str());}
+	{ print_error(__FILE__, __LINE__, __FUNCTION__, (what), (exit), std::format(EMPTY_STR __VA_ARGS__).c_str()); }
