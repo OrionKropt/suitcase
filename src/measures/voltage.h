966c17bd0bd4b01262db9325e1053cd47c2b8dbd
Kropt <SlavaCh2018@yandex.ru>
2024-05-21 18:44:58 +0500

    Added struct Max and Min for class Voltage

--- src/measures/voltage.h
+++ src/measures/voltage.h
@@ -1,30 +1,46 @@
 #pragma once
 #include <modbus.h>
 
+struct Max
+{
+	int16_t AB[2];
+	int16_t BC[2];
+	int16_t CA[2];
+	int16_t AN[2];
+	int16_t BN[2];
+	int16_t CN[2];
+};
+
+struct Min
+{
+	int16_t AB[2];
+	int16_t BC[2];
+	int16_t CA[2];
+	int16_t AN[2];
+	int16_t BN[2];
+	int16_t CN[2];
+};
+
 class Voltage
 {
-
-	uint16_t phase_3[2];
+public:
+	int16_t LL_3P_average[2];
+	int16_t LN_3P_average[2];
 
 	// line-line
-	uint16_t AB[2];
-	uint16_t BC[2];
-	uint16_t CA[2];
+	int16_t AB[2];
+	int16_t BC[2];
+	int16_t CA[2];
 
 	// line-neut
-	uint16_t AN[2];
-	uint16_t BN[2];
-	uint16_t CN[2];
+	int16_t AN[2];
+	int16_t BN[2];
+	int16_t CN[2];
 
-	// max AB
-	uint16_t AB_max[2];
+	// max 
+	Max max;
 
-	// min AB
-	uint16_t AB_min[2];
+	// min
+	Min min;
 
-	// max AN
-	uint16_t AN_max[2];
-
-	// min AN
-	uint16_t AN_min[2];
 };
