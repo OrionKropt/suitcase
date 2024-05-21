055f15e98393eb89895dc191c1449a091b3f1850
Kropt <SlavaCh2018@yandex.ru>
2024-05-21 18:43:47 +0500

    Added max and min values for BA, CA, BN, CN

--- src/measures/current.h
+++ src/measures/current.h
@@ -5,30 +5,30 @@ class Current
 {
 public:
 
-	uint16_t phase_3[2];
+	int16_t phase_3[2];
 
 	// Ampers per phase
-	uint16_t A[2];
-	uint16_t B[2];
-	uint16_t C[2];
-	uint16_t N[2];
+	int16_t A[2];
+	int16_t B[2];
+	int16_t C[2];
+	int16_t N[2];
 
 	// Ampers demand
-	uint16_t A_demand[2];
-	uint16_t B_demand[2];
-	uint16_t C_demand[2];
+	int16_t A_demand[2];
+	int16_t B_demand[2];
+	int16_t C_demand[2];
 
-	uint16_t A_demand_peak[2];
-	uint16_t B_demand_peak[2];
-	uint16_t C_demand_peak[2];
+	int16_t A_demand_peak[2];
+	int16_t B_demand_peak[2];
+	int16_t C_demand_peak[2];
 
-	uint16_t A_max[2];
-	uint16_t A_min[2];
+	int16_t A_max[2];
+	int16_t A_min[2];
 	
-	uint16_t B_max[2];
-	uint16_t B_min[2];
+	int16_t B_max[2];
+	int16_t B_min[2];
 
-	uint16_t C_max[2];
-	uint16_t C_min[2];
+	int16_t C_max[2];
+	int16_t C_min[2];
 	
 };
