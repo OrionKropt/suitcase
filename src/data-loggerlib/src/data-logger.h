44c1b56339740f8a8f74dce34a9c788a4e68047b
kropt <SlavaCh2018@yandex.ru>
2024-10-31 21:07:14 +0500

    Halloween. Add energy reading, fix file naming, change all structures for measuring value storage, remove Yoda conditions, add get_long function change the reading of all measuring values

--- src/data-loggerlib/src/data-logger.h
+++ src/data-loggerlib/src/data-logger.h
@@ -1,15 +1,12 @@
 #pragma once
 
-
 #include <modbus.h>
 #include "registers.h"
 #include "power.h"
 #include "current.h"
 #include "voltage.h"
+#include "energy.h"
 #include "setup.h"
-
-
-
 
 #define DEBUG
 
@@ -19,11 +16,8 @@ public:
 	DataLogger();
 	~DataLogger();
 	auto init() -> void;
-
 	auto read_data_from_device() -> int;
-
 	auto write_data_to_file() -> void const;
-
 	auto print_setup() -> void const;
 
 private:
@@ -33,22 +27,25 @@ private:
 	auto read_power_from_device(Power& power) -> int;
 	auto read_current_from_device() -> int;
 	auto read_voltage_from_device() -> int;
+	auto read_energy_from_device() -> int;
 	auto write_power_to_file(Power& power) -> void const;
 	auto write_demand_power_to_file(Power& power) -> void const;
 	auto write_current_to_file() -> void const;
 	auto write_voltage_to_file() -> void const;
+	auto write_energy_to_file() -> void const;
 
-	auto fast_pow(float n, int deg) -> float;
+	auto fast_pow(float n, int16_t deg) -> float;
+	inline auto get_long(uint16_t* src) -> long;
 	Setup setup;
 	Power real_power;
 	Power apparent_power;
 	Power reactive_power;
 	Current current;
 	Voltage voltage;
-
+	Energy energy;
 	modbus_t* ctx;
 
-	uint16_t buf[16]{ 0 };               // buffer for reading
+	uint16_t buf[16];               // buffer for reading
 
 	bool is_float;
 };
