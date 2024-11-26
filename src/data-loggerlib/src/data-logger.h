f1f9774377b43cf1b206892b6fbebddeb94f1a9c
kropt <SlavaCh2018@yandex.ru>
2024-11-26 22:23:06 +0500

    add read_power_factor_from_device() and write_power_factor_to_file()

--- src/data-loggerlib/src/data-logger.h
+++ src/data-loggerlib/src/data-logger.h
@@ -3,6 +3,7 @@
 #include <modbus.h>
 #include "registers.h"
 #include "power.h"
+#include "power-factor.h"
 #include "current.h"
 #include "voltage.h"
 #include "energy.h"
@@ -28,8 +29,10 @@ private:
 	auto read_current_from_device() -> int;
 	auto read_voltage_from_device() -> int;
 	auto read_energy_from_device() -> int;
+	auto read_power_factor_from_device() -> int;
 	auto write_power_to_file(Power& power) -> void const;
 	auto write_demand_power_to_file(Power& power) -> void const;
+	auto write_power_factor_to_file() -> void const;
 	auto write_current_to_file() -> void const;
 	auto write_voltage_to_file() -> void const;
 	auto write_energy_to_file() -> void const;
@@ -40,6 +43,7 @@ private:
 	Power real_power;
 	Power apparent_power;
 	Power reactive_power;
+	PowerFactor power_factor;
 	Current current;
 	Voltage voltage;
 	Energy energy;
